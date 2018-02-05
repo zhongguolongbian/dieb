3637e777ad097c1545915d69670be3169994bb3e
zhongguolongbian <1357663636@qq.com>
2018-02-05 18:15:55 +0800

    up

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+   $(".selection-right input").click(function(){
+        $(".selection-right").animate({
+            width:'350px'
+        });
+    });
+    $(".baby").click(function(){
+        $(".selection-right").animate({
+            width:'260px'
+        });
+    });
+
+
+
+});
